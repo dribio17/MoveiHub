da79f85f271e1b344497fd43e9ed96c3d0d420a3
Fabian Sharka <asedi739@gmail.com>
2025-12-18 15:08:11 +0100

    aggiunto tawk.to

--- app/layout.js
+++ app/layout.js
@@ -38,6 +38,20 @@ export default function RootLayout({ children }) {
           {/* root per i modali / player */}
         <div id="modal-root" />
         </main>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),
+                  s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/64b30db0cc26a871b028a303/default';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
